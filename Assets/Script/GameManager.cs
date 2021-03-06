6d27b83d93cbab4a4d189098b21c871750671ce9
Guillaume LEFEBVRE <gu.lefebvre@student.isartdigital.com>
2021-03-07 00:28:00 +0100

    start of project

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public TMP_InputField inputField = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField.Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Submit"))
+        {
+            
+        }
+        
+    }
+}
---
+++ Assets/Script/MenuManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public void OnPlayClick()
+    {
+        SceneManager.LoadScene("Scenes/Game");
+    }
+    
+    public void OnQuitClick()
+    {
+        Application.Quit();
+    }
+}
