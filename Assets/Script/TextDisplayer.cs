62ff9dd83d88bcfd6f547e3462aa57513a379316
Hugo Chemouny <h.chemouny@student.isartdigital.com>
2021-04-07 13:28:49 +0200

    Text displayer for current and next words

---
+++ Assets/Script/TextDisplayer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextDisplayer : MonoBehaviour
+{
+    List<Text>     words    = new List<Text>();
+    List<Vector2>  wordsPos = new List<Vector2>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Text T in GetComponentsInChildren<Text>())
+        {
+            words.Add(T);
+            wordsPos.Add(T.rectTransform.position);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+            NextWord("OGBWOin");
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (!words[i].rectTransform.position.Equals(wordsPos[i]))
+            {
+                words[i].rectTransform.position = new Vector3(
+                Mathf.Lerp(words[i].rectTransform.position.x, wordsPos[i].x, 0.01f),
+                Mathf.Lerp(words[i].rectTransform.position.y, wordsPos[i].y, 0.01f), 0);
+            }
+
+            if (!words[i].transform.localScale.Equals(Vector3.one))
+            {
+                words[i].transform.localScale =
+                new Vector3(Mathf.Lerp(words[i].transform.localScale.x, 1f, 0.01f), Mathf.Lerp(words[i].transform.localScale.y, 1f, 0.01f), 1);
+            }
+        }
+    }
+
+    void NextWord(string newWord)
+    {
+        words[3].rectTransform.position = words[2].rectTransform.position;
+        words[3].text                   = words[2].text;
+        words[3].transform.localScale   = words[2].transform.localScale / 2;
+
+        words[2].rectTransform.position = words[1].rectTransform.position;
+        words[2].text                   = words[1].text;
+        words[2].transform.localScale   = words[1].transform.localScale;
+
+        words[1].rectTransform.position = words[0].rectTransform.position;
+        words[1].text                   = words[0].text;
+        words[1].transform.localScale   = words[0].transform.localScale;
+
+        words[0].rectTransform.position += new Vector3(0, 20, 0);
+        words[0].text                   = newWord;
+        words[0].transform.localScale   = new Vector3(0f, 0f, 1);
+    }
+}
